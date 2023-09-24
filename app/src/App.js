228b9ffec72a2ea3be4714eb40fa810662ac0a8b
gpaparaz <giorgia.paparazzo@gmail.com>
2023-09-24 21:20:40 +0200

    add props -> communication from parent to child
    un modo per passare delle chiavi e dei valori assiciati  tramite props è dichiarare parametro e valore
    
    di fatto passo valori da parent a child

--- app/src/App.js
+++ app/src/App.js
@@ -3,18 +3,25 @@ import './App.css';
 import Product from './Product';
 
 function App() {
+  const primaCard = {
+    nome: 'Card Amazon',
+    prezzo: '14.00',
+    img: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+  };
+
+  const secondaCard = {
+    nome: 'Card Ebay',
+    prezzo: '13.50',
+    img: "https://plus.unsplash.com/premium_photo-1679913792906-13ccc5c84d44?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+  }
+
+
   return (
     <div className="App">
       <h2>la nostra prima card</h2>
       <div className='content'>
-
-      <Product/>
-
-      <Product/>
-
-      <Product/>
-
-      <Product/>
+      <Product nome={primaCard.nome} prezzo={primaCard.prezzo} img={primaCard.img}/>
+      <Product nome={secondaCard.nome} prezzo={secondaCard.prezzo} img={secondaCard.img} />
       </div>
     </div>
   );
--- app/src/Product.js
+++ app/src/Product.js
@@ -1,8 +1,10 @@
 import React from 'react'
-const img = "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
-const Product = () => {
-    const prezzo = 14.00;
+
+const Product = (props) => {
+    const prezzo = props.prezzo;
     const alt = "valore";
+    const nome = props.nome;
+    const img = props.img;
 
     const pStyle = {
         textTransform: 'uppercase'
@@ -12,7 +14,7 @@ const Product = () => {
     <article>
         <div className="card">
       <img src = {img} alt={alt} />
-            <h6>10€ amazon</h6>
+            <h6>{nome}</h6>
             <br/>
             <p>2,16 €</p>
 
