afa1b4ade5878799a37ca45e689689dc4156a6c9
gpaparaz <giorgia.paparazzo@gmail.com>
2023-09-24 13:54:44 +0200

    innested component

--- app/src/Componente1.js
+++ app/src/Componente1.js
@@ -2,10 +2,28 @@ import React from 'react'
 
 const Componente1 = () => {
   return (
-    <div>
-      Hello World
-    </div>
+    <section>
+        <div>
+            Hello World
+            <Saluto></Saluto>
+        </div>
+    </section>
   )
+}
+
+const Persona = () => {
+    return <h2>By Gio</h2>
+}
+
+const Messaggio = () => <h4>Sono un messaggio</h4>
+
+const Saluto = () => {
+    return (
+        <React.Fragment>
+            <Persona/>
+            <Messaggio/>
+        </React.Fragment>
+    )
 }
 
 export default Componente1
