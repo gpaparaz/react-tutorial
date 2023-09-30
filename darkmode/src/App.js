d17a5c05f91906a5768656a4c04bdf15347ded0d
gpaparaz <giorgia.paparazzo@gmail.com>
2023-09-30 15:23:59 +0200

    add list of articles

--- darkmode/src/App.js
+++ darkmode/src/App.js
@@ -1,12 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './data'
+import Articolo from './articolo';
+
+
+/* con spread operator passo ogni singolo elemento di data ad articolo*/
 
 function App() {
   return (
-    <div className="App">
-     Hello world
-     <button className="btn btn-primary">Click me</button>
-    </div>
+    <>
+    <section className="section-center">
+      <div className="container">
+        <button className="btn">
+          Cambia
+        </button>
+
+        <section className="article-section">
+          {data.map((el) => (
+            <Articolo key={el.id} {...el} />
+          ))}
+        </section>
+      </div>
+    </section>
+      </>
   );
 }
 
