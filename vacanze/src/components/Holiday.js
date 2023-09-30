62fbc446279a99dda72e48f37b49221a6e856c31
gpaparaz <giorgia.paparazzo@gmail.com>
2023-09-30 17:29:47 +0200

    added single holiday

--- vacanze/src/components/Holiday.js
+++ vacanze/src/components/Holiday.js
@@ -6,13 +6,19 @@ const url = "https://react--course-api.herokuapp.com/api/v1/data/vacanze";
 const Holiday = () => {
   const [data, setData] = useState([])
 
+  //il secondo state mi serve per passare l'array di oggetti
+  const [selected, setSelected] = useState(0)
+
   //funzione per fetchare i dati
   const getData = async () => {
     try{
       const response = await axios.get(url)
       //NB: faccio faccio la riga qui sotto, sto prendendo tutto l'oggetto data restituito dall'api
       setData(response.data)
+      console.log("response:")
       console.log(response)
+      console.log("response.data:")
+      console.log(response.data)
     }
     catch(e){
       console.log(e)
@@ -24,9 +30,20 @@ const Holiday = () => {
     getData()
   },[])
 
+  
   //return condizionale per verificare di aver risolto la promise
-if(data.success){
-  return <div>Vacanza</div>;
+  if(data.success){
+    return( <>
+  {
+    //ternary operator per controllare il numero di vacanze
+    //non so perchè questo pezzo di codice non funziona
+    // data.data.lenght > 0 ? <SingleHoliday {
+    //   ...data.data[selected]
+    // }/> : <h4>Niente vacanze</h4>
+    <SingleHoliday {...data.data[selected] } />
+
+  }
+  </>)
 }
 else return <h2>....Loading</h2>
 };
--- vacanze/src/components/SingleHoliday.js
+++ vacanze/src/components/SingleHoliday.js
@@ -1,8 +1,40 @@
 import React from "react";
 import { GrFormNextLink, GrFormPreviousLink } from "react-icons/gr";
 
-const SingleHoliday = () => {
-  return <div>Una Vacanza</div>;
+const SingleHoliday = ({
+  titolo,
+  descrizione,
+  durata,
+  img,
+  prezzo,
+  next,
+  prev,
+}) => {
+  return (
+    <div className="holiday-container">
+      <img src={img} alt={titolo} className="img" />
+      <div className="holiday-info">
+        <h4>{titolo}</h4>
+        <p>{descrizione}</p>
+        <div className="holiday-cost">
+          <small>{durata}</small>
+          <h5 style={{ color: "var(--primary-blue)" }}>
+            {(prezzo / 100).toFixed(2)} €
+          </h5>
+        </div>
+        <div className="btn-group">
+          <button type="button" className="btn btn-primary " onClick={prev}>
+            <GrFormPreviousLink className="icon" />{" "}
+          </button>
+  
+          <button type="button" className="btn btn-primary" onClick={next}>
+            {" "}
+            <GrFormNextLink className="icon" />{" "}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SingleHoliday;
