de662d20148c674b8a9847372d63d753368ecd49
gpaparaz <giorgia.paparazzo@gmail.com>
2023-09-30 15:30:55 +0200

    useEffect to change css class on html element
    lo scopo è attaccare le classi del css al tag html.
    per farlo uso lo useEffect.
    tuttavia qui mi cambia solo la classe, e quando refresho la pagina mi reimposta lo stato iniziale

--- darkmode/src/App.js
+++ darkmode/src/App.js
@@ -2,16 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 import data from './data'
 import Articolo from './articolo';
+import {useState, useEffect} from 'react'
 
 
 /* con spread operator passo ogni singolo elemento di data ad articolo*/
 
 function App() {
+  const [theme, setTheme] = useState('light-mode')
+
+  //aggiungo la classe prova al tag html 
+  document.documentElement.className = "prova"
+
+  //funzione che cambia il tema in base al valore
+  const cambiaTema = () => {
+    if(theme === 'light-mode')
+      setTheme('dark-mode')
+    else 
+      setTheme('light-mode')
+  }
+
+    console.log(theme)
+
+    //quando useEffect deve modificare la classe? ogni volta che cambia il valore di theme
+    //quindi ,[theme] serve a indicare che deve fare questa operazione quando cambia quel valore
+  useEffect(() => {
+    document.documentElement.className = theme
+  },[theme])
+
   return (
     <>
     <section className="section-center">
       <div className="container">
-        <button className="btn">
+        <button className="btn" onClick={cambiaTema}>
           Cambia
         </button>
 
