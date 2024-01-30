2b7f91ed1f0cb8b83583195f0cbcf2e10abeae4e
gpaparaz <giorgia.paparazzo@gmail.com>
2024-01-30 17:16:39 +0100

    useReducer pt 2

--- advanced-state-management/src/store/shopping-cart-context.jsx
+++ advanced-state-management/src/store/shopping-cart-context.jsx
@@ -12,6 +12,64 @@ export const CartContext = createContext({
 //ricreata ogni volta che la funzione componente viene eseguita, inoltre non avrà bisogno di 
 //accedere a oggetti di scena o altro
 function shoppingCartReducer(state, action) {
+    if(action.type === 'ADD_ITEM'){
+     //ora qui voglio restituire uno stato che è cambiato rispetto allo snapshot
+
+        // const updatedItems = [...prevShoppingCart.items];
+        //prevShoppingCart.items è ora state.items
+        const updatedItems = [...state.items];
+  
+        const existingCartItemIndex = updatedItems.findIndex(
+        //   (cartItem) => cartItem.id === id  // l'id può essere estratto da action
+        (cartItem) => cartItem.id === action.payload
+        );
+        const existingCartItem = updatedItems[existingCartItemIndex];
+  
+        if (existingCartItem) {
+          const updatedItem = {
+            ...existingCartItem,
+            quantity: existingCartItem.quantity + 1,
+          };
+          updatedItems[existingCartItemIndex] = updatedItem;
+        } else {
+          const product = DUMMY_PRODUCTS.find((product) => product.id === action.payload);
+          updatedItems.push({
+            id: action.payload,
+            name: product.title,
+            price: product.price,
+            quantity: 1,
+          });
+        }
+  
+        return {
+            //se avessi un oggetto più complesso potrei fare prima la copia di state con ...state e poi aggiornare quello che mi serve aggiornare
+          items: updatedItems,
+        };
+      
+    }
+
+    if(action.type === 'UPDATE_ITEM') {
+        const updatedItems = [...state.items];
+      const updatedItemIndex = updatedItems.findIndex(
+        (item) => item.id === action.payload.productId
+      );
+
+      const updatedItem = {
+        ...updatedItems[updatedItemIndex],
+      };
+
+      updatedItem.quantity += action.payload.amount;
+
+      if (updatedItem.quantity <= 0) {
+        updatedItems.splice(updatedItemIndex, 1);
+      } else {
+        updatedItems[updatedItemIndex] = updatedItem;
+      }
+
+      return {
+        items: updatedItems,
+      };
+    }
 
 
     //si restituisce lo stato aggiornato
@@ -20,9 +78,9 @@ function shoppingCartReducer(state, action) {
 
 //si occupa di gestire i dati di contesto e fornirli. tratterà il contesto relativo al carrello
 export default function CartContextProvider({ children }) {
-  const [shoppingCart, setShoppingCart] = useState({
-    items: [],
-  });
+//   const [shoppingCart, setShoppingCart] = useState({
+//     items: [],
+//   });
 
   /* invece di usare funzioni complesse che gestiscono gli stati uso un reducer.
   un reducer è una funzione che riduce uno o più valori complessi ad una semplice, ad esempio 
@@ -40,59 +98,26 @@ export default function CartContextProvider({ children }) {
 
 
   function handleAddItemToCart(id) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
 
-      const existingCartItemIndex = updatedItems.findIndex(
-        (cartItem) => cartItem.id === id
-      );
-      const existingCartItem = updatedItems[existingCartItemIndex];
+    /* invece di avere tutta questa logica complessa voglio usare il dispatch. l'azione 
+    passata può essere una stringa, un numero, ma di solito è un oggetto con proprietà come il
+    tipo o l'identificatore.
+    payload è un nome convenzionale che mi indica come ricevo il parametro
+    */
+    shoppingCartDispatch({
+        type:'ADD_ITEM', 
+        payload: id
+    })
 
-      if (existingCartItem) {
-        const updatedItem = {
-          ...existingCartItem,
-          quantity: existingCartItem.quantity + 1,
-        };
-        updatedItems[existingCartItemIndex] = updatedItem;
-      } else {
-        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
-        updatedItems.push({
-          id: id,
-          name: product.title,
-          price: product.price,
-          quantity: 1,
-        });
-      }
-
-      return {
-        items: updatedItems,
-      };
-    });
   }
 
   function handleUpdateCartItemQuantity(productId, amount) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-      const updatedItemIndex = updatedItems.findIndex(
-        (item) => item.id === productId
-      );
+    shoppingCartDispatch({
+        type: 'UPDATE_ITEM',
 
-      const updatedItem = {
-        ...updatedItems[updatedItemIndex],
-      };
-
-      updatedItem.quantity += amount;
-
-      if (updatedItem.quantity <= 0) {
-        updatedItems.splice(updatedItemIndex, 1);
-      } else {
-        updatedItems[updatedItemIndex] = updatedItem;
-      }
-
-      return {
-        items: updatedItems,
-      };
-    });
+        payload: { productId,
+        amount}
+    })
   }
 
   //qualsiasi componente wrappato all'interno di context può avere accesso a questi valori
