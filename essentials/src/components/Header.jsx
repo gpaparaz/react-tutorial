a86c7d7bb83fc18a170599f4b7d4641162e98159
gpaparaz <giorgia.paparazzo@gmail.com>
2024-01-30 12:10:19 +0100

    two way binding for input
    per farlo utilizzo gli stati nel component.
    posso scegliere di realizzare 4 stati diversi, uno per ciascun input, oppure uno unico che li gestisca tutti e 4 .
    posso scegliere di dichiarare 4 funzioni diverse, una per ciascun input, oppure una unica

--- essentials/src/components/Header.jsx
+++ essentials/src/components/Header.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import logo from '../assets/investment-calculator-logo.png'
+import React from "react";
+import logo from "../assets/investment-calculator-logo.png";
 
 const Header = () => {
   return (
-   <header id="header" >
-    <img src={logo} alt="investment calculator"/>
-    <h1>Investement calculator</h1>
-   </header>
-  )
-}
+    <header id="header">
+      <img src={logo} alt="investment calculator" />
+      <h1>Investement calculator</h1>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
