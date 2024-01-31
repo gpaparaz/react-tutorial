7c50682976a7c68fd706dfc8d21074d688d0c6c9
gpaparaz <giorgia.paparazzo@gmail.com>
2024-01-31 20:07:44 +0100

    refactor first API call

--- http-requests/src/components/AvailablePlaces.jsx
+++ http-requests/src/components/AvailablePlaces.jsx
@@ -1,8 +1,7 @@
-import { useState, useEffect } from 'react';
-import Places from './Places.jsx';
+import { useState, useEffect } from "react";
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
-
   const [availablePlaces, setAvailablePlaces] = useState([]);
   //tramite il fetch stiamo facendo una promise che tornerà valori diversi in base allo stato della promise
   /* potrei fare una cosa tipo 
@@ -14,16 +13,16 @@ export default function AvailablePlaces({ onSelectPlace }) {
   */
 
   useEffect(() => {
-    fetch('http://localhost:3000/places').then((response) => {
-      return response.json()
-    })
-    .then((resData) => {
-      setAvailablePlaces(resData.places)
-    })
+    async function fetchPlaces() {
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+    }
 
-  }, [])
+    fetchPlaces();
+  }, []);
 
-  console.log(availablePlaces)
+  console.log(availablePlaces);
 
   return (
     <Places
