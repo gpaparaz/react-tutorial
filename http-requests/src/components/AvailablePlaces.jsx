1556766a6c00c8fef9db0e069237ad56275c1e4a
gpaparaz <giorgia.paparazzo@gmail.com>
2024-01-31 20:04:59 +0100

    add first API call with useEffect

--- http-requests/src/components/AvailablePlaces.jsx
+++ http-requests/src/components/AvailablePlaces.jsx
@@ -1,12 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
 
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  //tramite il fetch stiamo facendo una promise che tornerà valori diversi in base allo stato della promise
+  /* potrei fare una cosa tipo 
+  const places = await fetch('....') 
+  però per fare ciò avrei bisogno che la funzione AvailablePlaces sia async, e ciò non è permesso con 
+  i componenti di react 
+  tramite useEffect questa chiamata api verrà eseguita una sola volta, tramite l'array vuoto come dipendenza
+  verrà eseguita una sola volta
+  */
+
+  useEffect(() => {
+    fetch('http://localhost:3000/places').then((response) => {
+      return response.json()
+    })
+    .then((resData) => {
+      setAvailablePlaces(resData.places)
+    })
+
+  }, [])
+
+  console.log(availablePlaces)
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
