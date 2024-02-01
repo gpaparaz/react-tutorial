fc49adc20e5c9183fe3a083aa5f25e253fc9393e
gpaparaz <giorgia.paparazzo@gmail.com>
2024-02-01 22:08:05 +0100

    manage input with states

--- forms-user-inputs/src/components/Login.jsx
+++ forms-user-inputs/src/components/Login.jsx
@@ -1,17 +1,44 @@
+import { useState } from "react";
+
 export default function Login() {
+
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: ''
+  })
+
+  function handleSubmit(event) {
+    /* permette di non inviare subito il form quando si clicca il bottone submit, in quanto il bottone 
+    stesso è di tipo submit */
+    event.preventDefault();
+    console.log(enteredValues)
+  }
+
+  function handleInputChange(identifier, event) {
+    setEnteredValues(prevValues => ({
+      ...prevValues,
+      [identifier]: event.target.value
+    }))
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" 
+          type="email" 
+          name="email" 
+          value={enteredValues.email}
+          onChange={(event) => handleInputChange('email', event)}/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" 
+          value={enteredValues.password}
+          onChange={(event) => handleInputChange('password', event)}/>
         </div>
       </div>
 
